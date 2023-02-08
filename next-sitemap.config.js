5d65a472131fa2f5bec4704d02ceffe28e438c4d
Teddy <devted19@gmail.com>
2023-02-08 06:43:44 +0200

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://frontendted.co.za",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  generateIndexSitemap: false,
+  autoLastmod: true,
+  changefreq: "daily",
+  priority: 0.7,
+};
