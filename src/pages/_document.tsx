2a2b57165cc979de574b0257f74c1f73141f029b
Teddy <devted19@gmail.com>
2023-02-08 06:52:35 +0200

    site verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,13 @@ import { CssBaseline } from "@nextui-org/react";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>{CssBaseline.flush()}</Head>
+      <Head>
+        {CssBaseline.flush()}
+        <meta
+          name="google-site-verification"
+          content="EP6VWzh0BkTlBj0c5iHBHkRpCrpWmDQ43L5Nix5LnS4"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
